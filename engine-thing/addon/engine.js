45ebb90fa20a3871c0b91a0870b3499580f7db3b
Bryan Levay <blevay@linkedin.com>
2016-06-06 14:51:29 -0700

    configured engine

--- engine-thing/addon/engine.js
+++ engine-thing/addon/engine.js
@@ -2,7 +2,7 @@ import Engine from 'ember-engines/engine';
 import Resolver from 'ember-engines/resolver';
 import loadInitializers from 'ember-load-initializers';
 
-const modulePrefix = 'dat-engine-tho';
+const modulePrefix = 'engine-thing';
 const Eng = Engine.extend({
   modulePrefix,
   Resolver
--- engine-thing/index.js
+++ engine-thing/index.js
@@ -3,6 +3,7 @@ var EngineAddon = require('ember-engines/lib/engine-addon');
 
 module.exports = EngineAddon.extend({
   name: 'engine-thing',
+  included: [],
 
   isDevelopingAddon: function() {
     return true;
