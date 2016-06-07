402b6fbe3d683a46db2a0faf562826963d2f0baf
Bryan Levay <blevay@linkedin.com>
2016-06-06 17:34:45 -0700

    fix name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Test-engine
+# engine-test
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'test-engine',
+    modulePrefix: 'engine-test',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
