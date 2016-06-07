3dfe299c011c22c088dd186a057985cb33f9bdfc
Bryan Levay <blevay@linkedin.com>
2016-06-06 17:55:40 -0700

    components

---
+++ that-engine/addon/components/parent.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+export default Ember.Component.extend({
+  init() {
+    this.openChildren = [{name: 'John'}, {name: 'Paul'}, {name: 'George'}]
+  },
+
+  willUpdate() {
+    // i'm never called
+    console.log('updating...');
+  },
+
+  willDestroyElement() {
+    // i'm never called
+    console.log('destroying...');
+  },
+
+  actions: {
+    openChild(child = {name: "Ringo"}) {
+      let openChildren = this.get('openChildren');
+      openChildren.addObject(child);
+    },
+
+    closeChild(child) {
+      let openChildren = this.get('openChildren');
+      openChildren.removeObject(conversation);
+    }
+  }
+
+});
